d169a2ef27c19158210b185fc4dc01dc3df4701f
itsanurag9957 <anuragsutar33@gmail.com>
2025-03-01 03:28:48 +0530

    Added comments

--- hmsBackend/routes/api.php
+++ hmsBackend/routes/api.php
@@ -8,4 +8,6 @@ Route::post('/login', [LoginController::class, 'login']);
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('/logout', [LogOutController::class, 'logout']);
+
+    //all team members add your post/get routes in this middleware group function since all the links are protected in this group. Only authenticated user with valid authentication token will be able to access these links
 });
