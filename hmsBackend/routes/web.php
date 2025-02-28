2698e4d0ab8a0deaff6e6ab5ff65f634f394d17a
itsanurag9957 <anuragsutar33@gmail.com>
2025-03-01 03:24:03 +0530

    Comments addedin web.php and api.php

--- hmsBackend/routes/web.php
+++ hmsBackend/routes/web.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//instruction - do not use this route file since it is not needed as per our hms requirement. All the routes are to be added in the routes/api.php file for api creation
