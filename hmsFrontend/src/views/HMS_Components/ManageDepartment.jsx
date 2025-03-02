549458f12bcb24fde14865ec5c574cf351320503
itsanurag9957 <anuragsutar33@gmail.com>
2025-03-02 19:02:18 +0530

    Updated the routes in dashboard

---
+++ hmsFrontend/src/views/HMS_Components/ManageDepartment.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ManageDepartment = () => {
+  return <div>ManageDepartment</div>
+}
+
+export default ManageDepartment
--- hmsFrontend/src/views/HMS_Components/RegisterPatient.jsx
+++ hmsFrontend/src/views/HMS_Components/RegisterPatient.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const RegisterPatient = () => {
-  return <div>Register patient component</div>
+  return <div>RegisterPatient</div>
 }
 
 export default RegisterPatient
