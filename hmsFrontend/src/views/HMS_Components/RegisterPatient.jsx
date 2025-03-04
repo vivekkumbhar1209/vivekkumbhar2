0c5516cb54fa7d06735e077638a200990a3472b0
itsanurag9957 <anuragsutar33@gmail.com>
2025-03-04 12:51:14 +0530

    Solved the patient resgitration error

--- hmsFrontend/src/views/HMS_Components/RegisterPatient.jsx
+++ hmsFrontend/src/views/HMS_Components/RegisterPatient.jsx
@@ -2,41 +2,43 @@ import React from 'react'
 import { useState } from 'react'
 import axios from 'axios'
 
-/*const RegisterPatient = () => {
+const RegisterPatient = () => {
+  // const [data, setData] = useState({
+  //   username: '',
+  //   password: '',
+  // })
 
-  const [data , setData] = useState({
-    username:'',
-    password:''
-  })
+  // const handleChange = (e) => {
+  //   setData({ ...data, [e.target.name]: e.target.value })
+  //   console.log(data)
+  // }
 
-  const handleChange=(e)=>{
-    setData({...data,[e.target.name]:e.target.value})
-    console.log(data)
-  }
+  // const handleSubmit = (e) => {
+  //   e.preventDefault()
+  //   var token = localStorage.getItem('login-token')
+  //   console.log('form submit')
+  //   axios
+  //     .post('http://127.0.0.1:8000/api/demo', data, {
+  //       headers: {
+  //         Authorization: `Bearer ${token}`,
+  //       },
+  //     })
+  //     .then((res) => {
+  //       console.log(res.data)
+  //     })
+  // }
 
-  const handleSubmit=(e)=>{
-    e.preventDefault()
-    var token = localStorage.getItem('login-token')
-    console.log('form submit')
-    axios.post('http://127.0.0.1:8000/api/demo',data,{
-      headers:{
-        Authorization: `Bearer ${token}`
-      }
-    }).then(res=>{
-      console.log(res.data)
-    })
+  // return (
+  //   <>
+  //     <form onSubmit={handleSubmit}>
+  //       <input onChange={handleChange} type="text" placeholder="username" name="username" />
+  //       <input onChange={handleChange} type="password" placeholder="password" name="password" />
+  //       <input type="submit" />
+  //     </form>
+  //   </>
+  // )
 
-  }
-
-  return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <input onChange={handleChange} type='text' placeholder='username' name='username'/>  
-        <input onChange={handleChange} type='password' placeholder='password' name='password'/>
-        <input type='submit' />
-      </form> 
-    </>
-  )
+  return <h1>register patient form</h1>
 }
 
-export default RegisterPatient*/
+export default RegisterPatient
