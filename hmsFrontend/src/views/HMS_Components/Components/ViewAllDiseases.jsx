5dac564d9e9b5950960311f4abbafc9e390b73a0
Raksha-22 <raksha.ghevadekar@gmail.com>
2025-03-05 15:00:49 +0530

    Made changes

--- hmsFrontend/src/views/HMS_Components/Components/ViewAllDiseases.jsx
+++ hmsFrontend/src/views/HMS_Components/Components/ViewAllDiseases.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import {
+  CCard,
+  CCardHeader,
+  CInputGroup,
+  CFormInput,
+  CCardBody,
+  CButton,
+  CTable,
+  CTableHead,
+  CTableBody,
+  CTableRow,
+  CTableHeaderCell,
+  CTableDataCell,
+  CFormSelect,
+} from '@coreui/react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+
 
 const ViewAllDiseases = () => {
-  return <div>View All diseases</div>
+  return(<>
+   
+  </>)
 }
 
 export default ViewAllDiseases
