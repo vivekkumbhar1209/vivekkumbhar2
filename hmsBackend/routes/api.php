2a58c45c6b7bf77bc4dbd1f52c3e9f681e684685
Raksha-22 <raksha.ghevadekar@gmail.com>
2025-03-05 12:17:26 +0530

    Added styling in Doctor Registration Form

--- hmsBackend/routes/api.php
+++ hmsBackend/routes/api.php
@@ -25,7 +25,7 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('/registerDepartment', [DeptReg::class, 'registerDepartment']);
     Route::post('/updateDepartment', [UpdateDepartment::class, 'updateDepartment']);
     Route::post('/registerpatient', [PatientRegistration::class, 'registerPatient']);
-    Route::post('/registeruser', [UserRegistration::class, 'registerUser']);
+    Route::post('/registeruser', [UserRegistration::class, 'registerUser']);//this will register all users
     Route::get('/patients', [PatientController::class, 'index']);
     Route::get('/patients/{id}', [PatientController::class, 'show']);
     Route::get('/getDept', function () {
