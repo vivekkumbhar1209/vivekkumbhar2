3e2ad9a34910c0ef07f18bbb3666d1a79df8c3c3
Vivek <vivekkumbhar1802@gmail.com>
2025-03-10 13:40:41 +0530

    just a push for changes

--- hmsBackend/routes/api.php
+++ hmsBackend/routes/api.php
@@ -1,20 +1,19 @@
 <?php
 
 use App\Http\Controllers\AddDisease;
-use App\Http\Controllers\DiseaseController;
 use App\Http\Controllers\DepartmentController;
 use App\Http\Controllers\DeptReg;
-use App\Http\Controllers\DoctorController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\LogOutController;
 use App\Http\Controllers\MedicineCategoryController;
 use App\Http\Controllers\MedicineController;
-use App\Http\Controllers\OPDController;
 use App\Http\Controllers\PatientController;
 use App\Http\Controllers\PatientRegistration;
 use App\Http\Controllers\UpdateDepartment;
 use App\Http\Controllers\UserRegistration;
 use Illuminate\Support\Facades\Route;
+
+
 
 Route::post('/login', [LoginController::class, 'login']);
 
@@ -22,22 +21,26 @@ Route::middleware(['auth:sanctum'])->group(function () {
 
     //all team members add your post/get routes in this middleware group function since all the links are protected in this group. Only authenticated user with valid authentication token will be able to access these links
 
-    //Implementation is done for this api in the frontend
     Route::post('/logout', [LogOutController::class, 'logout']);
-    Route::post('/addDisease', [AddDisease::class, 'addDisease']);
+
+    //Implementation is done for this api in the frontend
+    Route::post('/adddisease', [AddDisease::class, 'addDisease']);
     Route::post('/registerDepartment', [DeptReg::class, 'registerDepartment']);
-    Route::get('/getDept', [DeptReg::class, 'getDept']);
     Route::post('/updateDepartment', [UpdateDepartment::class, 'updateDepartment']);
     Route::post('/registerpatient', [PatientRegistration::class, 'registerPatient']);
-    Route::put('/updateuser/{id}', [UserRegistration::class, 'updateUser']);
-    Route::post('/registeruser', [UserRegistration::class, 'registerUser']);//this will register all users
+    Route::post('/registeruser', [UserRegistration::class, 'registerUser']); //this will register all users
     Route::get('/patients', [PatientController::class, 'index']);
     Route::get('/patients/{id}', [PatientController::class, 'show']);
     Route::get('/viewAllUsers', [UserRegistration::class, 'getAllUsers']);
     Route::get('/getDept', [DeptReg::class, 'getDepartments']);
-    Route::get('/getMedicines', [MedicineController::class, "getMedicines"]);
-    Route::get('/getDept', [DepartmentController::class, 'allDepartment']);
+    Route::get('/getMedicineCategory', [MedicineCategoryController::class, 'getMedicineCategory']);
+    Route::get('/getMedicineCategories', [MedicineController::class, 'getMedicineCategories']);
     Route::post('/addMedicine', [MedicineController::class, 'addMedicine']);
+    Route::get('/medicines', [MedicineController::class, 'getMedicines']);
+    Route::post('/add-medicine', [MedicineController::class, 'addMedicine']);
+
+
+    Route::get('/getDept', [DepartmentController::class, 'allDepartment']);
 
 
 
@@ -45,13 +48,15 @@ Route::middleware(['auth:sanctum'])->group(function () {
     
     //Implementation is not done for these api in the frontend
     Route::post('/addMedicineCategory', [MedicineCategoryController::class, 'addCategory']);
-    Route::post('/getDoctorByDeparmentID', [DoctorController::class, 'getDoctorByDepartmentID']);
-    Route::post('/registerOPDPatient', [OPDController::class, 'registerOPDPatient']);
-    Route::get('/getdiseases', [AddDisease::class, 'getDisease']);
-    Route::get('/getPatientsWithOPDStatus', [OPDController::class, 'getPatientsWithOPDStatus']);
-  
-    //done by vivek 
+    //Additionally we need to create the following api as well
+    // 1. api which will send all the available medicines to the frontend
+    // 2. api which will send all the available diseases to the frontend
+    // 3. api which will send all the available departments to the frontend
+    // 4. api which will update the entries in the disease table based on the form data accepted from the frontend (form is created for this in frontend)
+    // 5. api which will add new disease in the disease table based on the form data accepted from the frontend (form is created for this in frontend)
+    
+
     Route::post('/getMedCategory', [MedicineCategoryController::class, 'getMedCategory']);
     Route::post('/addMedicine', [MedicineController::class, 'addMedicine']);
-
+    
 });
