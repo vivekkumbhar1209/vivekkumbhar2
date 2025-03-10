9eed2c22bc6004b5a6951e92db659cc0ee08205d
Dhanshree-123-bit <dhanshreemane099@gmail.com>
2025-03-10 13:10:26 +0530

    new edit dieases

--- hmsFrontend/src/App.js
+++ hmsFrontend/src/App.js
@@ -7,11 +7,11 @@ import './scss/style.scss'
 import './scss/examples.scss'
 import PrivateRoute from './PrivateRoute'
 
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
+// import { ToastContainer } from 'react-toastify'
+// import 'react-toastify/dist/ReactToastify.css'
 
 // Inside your JSX tree (e.g. end of return block):
-<ToastContainer position="top-right" autoClose={3000} />
+{/* <ToastContainer position="top-right" autoClose={3000} /> */}
 
 // Containers
 const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'))
