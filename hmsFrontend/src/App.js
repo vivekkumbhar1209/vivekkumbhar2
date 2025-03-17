73211bef6a18d4e57988608c3958096100c4cee7
itsanurag9957 <anuragsutar33@gmail.com>
2025-03-17 23:53:41 +0530

    Login commponent loading instantly

--- hmsFrontend/src/App.js
+++ hmsFrontend/src/App.js
@@ -18,7 +18,7 @@ const NavFooter = React.lazy(() => import('./website/src/assets/components/layou
 // Containers
 const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'))
 // Pages
-const Login = React.lazy(() => import('./views/pages/login/Login'))
+import Login from './views/pages/login/Login'
 const Register = React.lazy(() => import('./views/pages/register/Register'))
 const Page404 = React.lazy(() => import('./views/pages/page404/Page404'))
 const Page500 = React.lazy(() => import('./views/pages/page500/Page500'))
