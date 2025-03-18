23efe30a5c27bf8d924c416f30b2637b530a6028
itsanurag9957 <anuragsutar33@gmail.com>
2025-03-18 17:39:05 +0530

    Added forgot password module and added new loaders

--- hmsBackend/routes/api.php
+++ hmsBackend/routes/api.php
@@ -70,3 +70,6 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('/getProfilePhoto', [OPDController::class, 'showProfilePhoto']);
 
 });
+
+Route::post('/forgot-password', [UserRegistration::class, 'forgotPassword']);
+Route::post('/update-password', [UserRegistration::class, 'updatePassword']);
