915cede6329a87ab258122dd5440b080b5fdad45
itsanurag9957 <anuragsutar33@gmail.com>
2025-03-19 20:49:41 +0530

    Implemented api calls using the base url code and added a loader in the enquiry.jsx form

--- hmsFrontend/src/website/src/assets/components/enquiry form/enquiry.jsx
+++ hmsFrontend/src/website/src/assets/components/enquiry form/enquiry.jsx
@@ -1,10 +1,14 @@
 import { FaPhone, FaInstagram, FaXTwitter, FaLinkedinIn, FaFacebookF, FaYoutube, FaLocationDot } from "react-icons/fa6";
-import enquirybg from"../../../../Images/enquirybg.jpg";
+import enquirybg from "../../../../Images/enquirybg.jpg";
 import { useState } from 'react';
 import axios from 'axios';
 import Swal from 'sweetalert2';
+import Ringloader from '../../../../../components/RingLoader'
+import api from '../../../../../api'
 
 const EnquiryForm = () => {
+
+  const [loading, setLoading] = useState(false)
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -15,11 +19,12 @@ const EnquiryForm = () => {
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
-  
+
   const handleSubmit = async (e) => {
+    setLoading(true)
     e.preventDefault();
     try {
-      const response = await axios.post('http://127.0.0.1:8000/api/submit-enquiry', formData);
+      const response = await api.post('/submit-enquiry', formData);
       Swal.fire({
         icon: 'success',
         title: 'Success!',
@@ -35,6 +40,8 @@ const EnquiryForm = () => {
         text: 'Failed to submit enquiry. Please try again.',
         confirmButtonColor: '#d33', // Red button color
       });
+    } finally {
+      setLoading(false)
     }
   };
 
@@ -74,13 +81,17 @@ const EnquiryForm = () => {
             <textarea name="message" value={formData.message} onChange={handleChange} placeholder="Enter your message here" className="w-full border-b border-gray-400 p-2 focus:outline-none focus:border-black" rows="2"></textarea>
           </div>
           <button type="submit" className="block mt-6 bg-purple-900 text-white px-6 py-3 rounded-lg font-semibold hover:opacity-90 transition mb-0">
-            Submit
+            {loading ? (
+              <Ringloader />
+            ) : (
+              'Submit'
+            )}
           </button>
         </form>
       </div>
-      
+
       {/* Contact Us */}
-      <div className="bg-white p-6 md:p-10 rounded-2xl shadow-lg w-full md:w-1/3 bg-opacity-90 mx-auto md:mx-0">     
+      <div className="bg-white p-6 md:p-10 rounded-2xl shadow-lg w-full md:w-1/3 bg-opacity-90 mx-auto md:mx-0">
         <h2 className="text-2xl font-semibold mb-6">Contact Us</h2>
         <div className="space-y-6">
           {/* Phone Section */}
@@ -93,7 +104,7 @@ const EnquiryForm = () => {
               <p className="text-gray-700">+91 88888 22222</p>
             </div>
           </div>
-          
+
           {/* Social Media Section */}
           <div>
             <p className="font-medium">Follow us on</p>
@@ -105,7 +116,7 @@ const EnquiryForm = () => {
               <FaYoutube className="text-black text-2xl cursor-pointer" />
             </div>
           </div>
-          
+
           {/* Address Section */}
           <div className="flex items-center space-x-4">
             <div className="w-12 h-12 bg-gray-200 rounded-full flex items-center justify-center">
